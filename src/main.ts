6cc75daef11c8c7721c387ab913daa92407a5252
negri234279 <rafalejo@live.com>
2022-09-18 23:26:17 +0200

    👔 Add prefix and versioning api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    app.setGlobalPrefix('api')
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    })
+
     await app.listen(3000)
 }
 bootstrap()
