48a5956a312aa87053dc26993eda39c5347792f8
negri234279 <rafalejo@live.com>
2022-10-31 23:45:10 +0100

    👔 Serialixer user profile

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common'
+import {
+    Controller,
+    Get,
+    Request,
+    UseGuards,
+    UseInterceptors,
+    ClassSerializerInterceptor,
+} from '@nestjs/common'
 import { JwtAuthGuard } from 'src/auth/application/guards/jwt-auth.guard'
 import { RequestAuth } from 'src/shared/domain/@types/RequestAuth'
 import { UserUseCase } from '../application/useCase.service'
-import { UserMapper } from './user.mapper'
+import { UserProfile } from './serializers'
 
 @Controller('auth')
 export class UsersController {
     constructor(private userUseCase: UserUseCase) {}
 
     @UseGuards(JwtAuthGuard)
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('profile')
     async getProfile(@Request() req: RequestAuth) {
         const user = await this.userUseCase.profile(req.user.id)
 
-        return UserMapper.toDto(user)
+        return new UserProfile(user)
     }
 }
