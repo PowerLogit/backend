a6155aae8549c0e01e9f31c854d8ee299421c0cc
negri234279 <rafalejo@live.com>
2022-11-01 23:03:29 +0100

    ♻️ Move signToken to controller

--- src/auth/application/usesCases/login.service.ts
+++ src/auth/application/usesCases/login.service.ts
@@ -1,9 +1,5 @@
 import { ForbiddenException, Injectable } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
-import { BearerPayload } from 'src/auth/domain/@types/BearerPayload'
-import { AccessToken } from 'src/auth/domain/@types/jwt'
 import { LoginUserDto } from 'src/auth/infrastructure/dtos'
-import { UserRol } from 'src/users/domain/@types/user'
 import { UserModel } from 'src/users/domain/user.model'
 import { EmailVO } from 'src/users/domain/vo/email.vo'
 import { PasswordVO } from 'src/users/domain/vo/password.vo'
@@ -11,15 +7,12 @@ import { UserRepository } from 'src/users/infrastructure/repository.service'
 
 @Injectable()
 export class LoginUseCase {
-    constructor(
-        private userRepo: UserRepository,
-        private jwtService: JwtService,
-    ) {}
+    constructor(private userRepo: UserRepository) {}
 
     public async execute({
         email,
         password,
-    }: LoginUserDto): Promise<AccessToken> {
+    }: LoginUserDto): Promise<UserModel> {
         const userEmail = EmailVO.create(email)
         const userPassword = PasswordVO.createPlain(password)
 
@@ -30,18 +23,6 @@ export class LoginUseCase {
         if (!isValidPassword)
             throw new ForbiddenException('Credentials incorrect')
 
-        return await this.signToken(user)
-    }
-
-    private async signToken(user: UserModel): Promise<AccessToken> {
-        const payload: BearerPayload = {
-            id: user.id.value,
-            name: user.name.value,
-            rol: user.rol.value as UserRol,
-        }
-
-        const access_token = await this.jwtService.signAsync(payload)
-
-        return { access_token }
+        return user
     }
 }
--- src/users/domain/vo/rol.vo.ts
+++ src/users/domain/vo/rol.vo.ts
@@ -1,23 +1,18 @@
 import { VOFormatException } from 'src/shared/domain/errors/voFormatException'
 import { ValueObject } from 'src/shared/domain/vo/valueObject'
+import { USER_ROL } from '../@types/UserRol'
 
 export class RolVO extends ValueObject<string> {
     public equals(valueObject: RolVO): boolean {
         return this.value === valueObject.value
     }
 
-    protected assertIsValid(value: string): void | VOFormatException {
-        if (!(<any>Object).values(USER_ROL).includes(value))
+    protected assertIsValid(value: USER_ROL): void {
+        if (!Object.values(USER_ROL).includes(value))
             throw new VOFormatException(RolVO.name, value)
     }
 
     public static create(value: string): RolVO {
         return new RolVO(value)
     }
-}
-
-export type UserRol = 'athlete' | 'coach'
-export enum USER_ROL {
-    ATHLETE = 'athlete',
-    COACH = 'coach',
 }
