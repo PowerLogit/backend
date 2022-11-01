ac7fc10db1e3ed2e4456fe42d6075f8e0cb3e225
negri234279 <rafalejo@live.com>
2022-11-02 00:44:03 +0100

    👔 Add feature to update user

--- src/users/domain/users.module.ts
+++ src/users/domain/users.module.ts
@@ -4,11 +4,13 @@ import { UserEntity } from '../infrastructure/user.entity'
 import { UserRepository } from '../infrastructure/repository.service'
 import { UserProfileController } from '../infrastructure/controllers/profile.comtroller'
 import { UserProfileUseCase } from '../application/useCases/profile.service'
+import { UpdateUserController } from '../infrastructure/controllers'
+import { UpdateUserUseCase } from '../application/useCases/updateUser.service'
 
 @Module({
-    providers: [UserProfileUseCase, UserRepository],
+    providers: [UserProfileUseCase, UpdateUserUseCase, UserRepository],
     imports: [TypeOrmModule.forFeature([UserEntity])],
     exports: [TypeOrmModule],
-    controllers: [UserProfileController],
+    controllers: [UserProfileController, UpdateUserController],
 })
 export class UsersModule {}
